5f05d39975df03b89777534614cb9d75c558f65f
Vladimir Rodchenko <vladimir@rodchenko.by>
2018-11-28 18:31:21 +0300

    added test for StartupBase

---
+++ test/Aurochses.Database.EntityFrameworkCore.Tests/Fakes/FakeStartupBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Aurochses.Database.EntityFrameworkCore.Tests.Fakes
+{
+    public class FakeStartupBase : StartupBase
+    {
+        public FakeStartupBase(IConfiguration configuration)
+            : base(configuration)
+        {
+
+        }
+    }
+}
---
+++ test/Aurochses.Database.EntityFrameworkCore.Tests/StartupBaseTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Aurochses.Database.EntityFrameworkCore.Tests.Fakes;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Aurochses.Database.EntityFrameworkCore.Tests
+{
+    public class StartupBaseTests
+    {
+        [Fact]
+        public void Configuration_Get_Success()
+        {
+            // Arrange
+            IConfiguration configuration = new ConfigurationRoot(new List<IConfigurationProvider>());
+
+            var startupBase = new FakeStartupBase(configuration);
+
+            // Act & Assert
+            Assert.Equal(configuration, startupBase.Configuration);
+        }
+
+        [Fact]
+        public void ConfigureServices_Success()
+        {
+            // Arrange & Act & Assert
+            new FakeStartupBase(null).ConfigureServices(new ServiceCollection());
+        }
+
+        [Fact]
+        public void Configure_Success()
+        {
+            // Arrange & Act & Assert
+            new FakeStartupBase(null).Configure();
+        }
+    }
+}
